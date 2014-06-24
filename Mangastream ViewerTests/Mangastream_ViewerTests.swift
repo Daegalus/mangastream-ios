550033bd3102ace0d113a28121ee2fc9dbb68121
Yulian Kuncheff <yulian@Yulians-MacBook-Air.local>
2014-06-24 12:00:02 -0700

    Initial Commit

---
+++ Mangastream ViewerTests/Mangastream_ViewerTests.swift
@@ -0,0 +1,35 @@
+//
+//  Mangastream_ViewerTests.swift
+//  Mangastream ViewerTests
+//
+//  Created by Yulian Kuncheff on 6/24/14.
+//  Copyright (c) 2014 Yulian Kuncheff. All rights reserved.
+//
+
+import XCTest
+
+class Mangastream_ViewerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
